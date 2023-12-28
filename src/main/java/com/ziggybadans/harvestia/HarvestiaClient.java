17e70c9bf78177e9718a9cf10d785a7c3481140b
ziggybadans <ziggyspike@hotmail.com.au>
2023-12-28 22:22:15 +1100

    Initial commit

---
+++ src/main/java/com/ziggybadans/harvestia/HarvestiaClient.java
@@ -0,0 +1,10 @@
+package com.ziggybadans.harvestia;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HarvestiaClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/ziggybadans/harvestia/HarvestiaDataGenerator.java
@@ -0,0 +1,11 @@
+package com.ziggybadans.harvestia;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class HarvestiaDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
